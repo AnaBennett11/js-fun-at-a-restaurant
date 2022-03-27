ad30e5d496f548eb3a488d77d72b8a919a3ccd10
Ana Bennett <anabennett1111@gmail.com>
2022-03-26 20:34:03 -0500

    src/meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
+function nameMenuItem(pizza) {
+return `Delicious ${pizza}`
+};
 
+function createMenuItem(itemName, itemPrice, itemType) {
+  var menuItemName ={
+  name: itemName,
+  price: itemPrice,
+  type: itemType,
+  }
+  return menuItemName
 }
+//ingredient has argument of cheese, listOfIngredients = empty array
+function addIngredients(ingredient,listOfIngredients){
+  if(listOfIngredients.includes(ingredient)) {
+  return ingredient;
+} else {
+    return listOfIngredients.push(ingredient);
+  }
+}
+
+function formatPrice(initialPrice) {
+  return "$" + initialPrice;
+}
+
+function decreasePrice(price) {
+  return price * .9;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  return{
+    title:title,
+    ingredients:ingredients,
+    type:menuItemType,
+  }
+}
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
